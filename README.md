3e3065db6daaf37dccb7947b69d2b98aa8a7b4c1
Tinker Twins <tinkertwinsofficial@gmail.com>
2023-05-23 23:29:26 -0400

    Remove history to reduce repo size

---
+++ README.md
@@ -0,0 +1,179 @@
+# AutoDRIVE Nigel Dataset
+
+[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.7964080.svg)](https://doi.org/10.5281/zenodo.7964080)
+
+### Dynamics and Perception Dataset of AutoDRIVE Ecosystem's Nigel Vehicle
+
+:warning: ***Repository Size:** This repository is about 66 GB in size. This repository uses [Git LFS](https://git-lfs.com/). Please check your internet data plan and local disk space before cloning this repository.*
+
+<table>
+<thead>
+  <tr>
+    <th align="center"><img src="straight_30_hz/straight.png"></th>
+    <th align="center"><img src="skidpad_30_hz/skidpad.png"></th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td align="center">Straight Track</td>
+    <td align="center">Skidpad Track</td>
+  </tr>
+  <tr>
+    <td align="center"><img src="fishhook_30_hz/fishhook.png"></td>
+    <td align="center"><img src="slalom_30_hz/slalom.png"></td>
+  </tr>
+  <tr>
+    <td align="center">Fishhook Track</td>
+    <td align="center">Slalom Track</td>
+  </tr>
+  <tr>
+    <td  align="center" colspan="2"><img src="eight_30_hz/eight.png"></td>
+  </tr>
+  <tr>
+    <td  align="center" colspan="2">Eight Track</td>
+  </tr>
+</tbody>
+</table>
+
+This repository uses [AutoDRIVE Ecosystem](https://autodrive-ecosystem.github.io/) to capture data from a 1:14 scale Ackerman-steered vehicle called Nigel. The source repository for AutoDRIVE Ecosystem can be found [here](https://github.com/Tinker-Twins/AutoDRIVE).
+
+## Dataset Structure:
+
+The vehicle dataset comprises the following:
+
+| **DATA** | timestamp | throttle |	steering | leftTicks | rightTicks |	posX | posY |	posZ | roll |	pitch |	yaw |	speed |	angX |	angY |	angZ | accX |	accY | accZ |	cam0 | cam1 | lidar |
+| -------- | --------- | -------- |	-------- | --------- | ---------- |	---- | ---- |	---- | ---- |	----- |	--- |	----- |	---- |	---- |	---- | ---- |	---- | ---- |	---- | ---- | ----- |
+| **UNIT** | yyyy_MM_dd_HH_mm_ss_fff | norm% | rad | count | count |	m | m |	m | rad |	rad |	rad |	m/s |	rad/s |	rad/s |	rad/s | m/s^2 |	m/s^2 | m/s^2 |	img_path | img_path | array(float) |
+
+The traffic light dataset (only applicable for Eight Track) comprises the following:
+
+| **DATA** | timestamp | state |
+| -------- | --------- | ----- |
+| **UNIT** | yyyy_MM_dd_HH_mm_ss_fff | int{0=disabled,1=red,2=yellow,3=green} |
+
+## Vehicle Parameters:
+- Wheelbase (m): 0.1415
+- Track width (m): 0.1530
+- Throttle Limit (norm%): 1.0000
+- Steering Limit (rad): 0.5236
+- Linear Velocity Limit (m/s): 0.2670
+- Angular Velocity Limit (rad/s): 0.8051
+- [Throttle vs. Velocity Mapping](https://github.com/Tinker-Twins/AutoDRIVE-Nigel-Dataset/blob/main/vehicle_parameters/Nigel_Throttle_Velocity_Mapping.xlsx):
+![](https://github.com/Tinker-Twins/AutoDRIVE-Nigel-Dataset/blob/main/vehicle_parameters/Nigel_Throttle_Velocity_Mapping.png)
+
+## Automated Data Collection:
+
+The [`open_loop_control.py`](https://github.com/Tinker-Twins/AutoDRIVE-Nigel-Dataset/blob/main/open_loop_controller/open_loop_control.py) script makes use of [AutoDRIVE Devkit's](https://github.com/Tinker-Twins/AutoDRIVE/tree/AutoDRIVE-Devkit) [Python API](https://github.com/Tinker-Twins/AutoDRIVE/tree/AutoDRIVE-Devkit/ADSS%20Toolkit/autodrive_py). The script is capable of selecting a maneuver and its direction, and controlling the vehicle actuators within the prescribed limits in an open-loop setting.
+
+```bash
+python3 open_loop_control.py --maneuver={straight, skidpad, fishhook, slalom} --direction={cw, ccw} --throttle=[-1, 1] --steering=[0, 0.5236] --throttle_noise=[0, 0.001] --steering_noise=[0, 0.001]
+```
+
+**Control Input Variations:**
+
+- **Throttle Gradations (norm%):** 0.2, 0.4, 0.6, 0.8, 1.0 (straight maneuver has additional throttle gradations: 0.1, 0.3, 0.5, 0.7, 0.9)
+- **Steering Gradations (rad):** 0.1047, 0.2094, 0.3142, 0.4189, 0.5236 (straight maneuver does not have any steering gradations)
+
+## Data Visualization
+
+[Single Maneuver Data Visualization](https://github.com/Tinker-Twins/AutoDRIVE-Nigel-Dataset/blob/main/data_visualization/data_visualization_single.ipynb)
+
+<table>
+<thead>
+  <tr>
+    <th align="center"><img src="data_visualization/straight_maneuver_single.png"></th>
+    <th align="center"><img src="data_visualization/skidpad_maneuver_single.png"></th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td align="center">Straight Maneuver</td>
+    <td align="center">Skidpad Maneuver</td>
+  </tr>
+  <tr>
+    <td align="center"><img src="data_visualization/fishhook_maneuver_single.png"></td>
+    <td align="center"><img src="data_visualization/slalom_maneuver_single.png"></td>
+  </tr>
+  <tr>
+    <td align="center">Fishhook Maneuver</td>
+    <td align="center">Slalom Maneuver</td>
+  </tr>
+  <tr>
+    <td  align="center" colspan="2"><img src="data_visualization/eight_maneuver_single.png"></td>
+  </tr>
+  <tr>
+    <td  align="center" colspan="2">Eight Maneuver</td>
+  </tr>
+</tbody>
+</table>
+
+[Collective Maneuver Data Visualization](https://github.com/Tinker-Twins/AutoDRIVE-Nigel-Dataset/blob/main/data_visualization/data_visualization_collective.ipynb)
+
+<table>
+<thead>
+  <tr>
+    <th align="center"><img src="data_visualization/straight_maneuver_collective.png"></th>
+    <th align="center"><img src="data_visualization/skidpad_maneuver_collective.png"></th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td align="center">Straight Maneuver</td>
+    <td align="center">Skidpad Maneuver</td>
+  </tr>
+  <tr>
+    <td align="center"><img src="data_visualization/fishhook_maneuver_collective.png"></td>
+    <td align="center"><img src="data_visualization/slalom_maneuver_collective.png"></td>
+  </tr>
+  <tr>
+    <td align="center">Fishhook Maneuver</td>
+    <td align="center">Slalom Maneuver</td>
+  </tr>
+  <tr>
+    <td align="center"><img src="data_visualization/eight_maneuver_collective.png"></td>
+    <td align="center"><img src="data_visualization/all_maneuvers_collective.png"></td>
+  </tr>
+  <tr>
+    <td align="center">Eight Maneuver</td>
+    <td align="center">All Maneuvers</td>
+  </tr>
+</tbody>
+</table>
+
+## Citation
+
+We encourage you to read and cite the following papers if you use any part of this dataset for your research:
+
+#### [AutoDRIVE: A Comprehensive, Flexible and Integrated Cyber-Physical Ecosystem for Enhancing Autonomous Driving Research and Education](https://arxiv.org/abs/2212.05241)
+```bibtex
+@eprint{AutoDRIVE-Ecosystem-2022,
+doi = {10.48550/ARXIV.2212.05241},
+url = {https://arxiv.org/abs/2212.05241},
+author = {Samak, Tanmay Vilas and Samak, Chinmay Vilas and Kandhasamy, Sivanathan and Krovi, Venkat and Xie, Ming},
+keywords = {Robotics (cs.RO), FOS: Computer and information sciences, FOS: Computer and information sciences},
+title = {AutoDRIVE: A Comprehensive, Flexible and Integrated Cyber-Physical Ecosystem for Enhancing Autonomous Driving Research and Education},
+publisher = {arXiv},
+year = {2022},
+copyright = {arXiv.org perpetual, non-exclusive license}
+}
+```
+
+#### [AutoDRIVE Simulator: A Simulator for Scaled Autonomous Vehicle Research and Education](https://arxiv.org/abs/2103.10030)
+```bibtex
+@inproceedings{AutoDRIVE-Simulator-2021,
+author = {Samak, Tanmay Vilas and Samak, Chinmay Vilas and Xie, Ming},
+title = {AutoDRIVE Simulator: A Simulator for Scaled Autonomous Vehicle Research and Education},
+year = {2021},
+isbn = {9781450390453},
+publisher = {Association for Computing Machinery},
+address = {New York, NY, USA},
+url = {https://doi.org/10.1145/3483845.3483846},
+doi = {10.1145/3483845.3483846},
+booktitle = {2021 2nd International Conference on Control, Robotics and Intelligent System},
+pages = {1–5},
+numpages = {5},
+location = {Qingdao, China},
+series = {CCRIS'21}
+}
+```
+This work has been published in **2021 International Conference on Control, Robotics and Intelligent System (CCRIS).** The publication can be found on [ACM Digital Library](https://dl.acm.org/doi/abs/10.1145/3483845.3483846).
---
+++ open_loop_controller/autodrive.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+
+# Import libraries
+import numpy as np
+import base64
+from io import BytesIO
+from PIL import Image
+import cv2
+
+################################################################################
+
+# Vehicle class
+class Vehicle:
+    def __init__(self):
+        # Vehicle data
+        self.id                       = None
+        self.throttle                 = None
+        self.steering                 = None
+        self.encoder_ticks            = None
+        self.encoder_angles           = None
+        self.position                 = None
+        self.orientation_quaternion   = None
+        self.orientation_euler_angles = None
+        self.angular_velocity         = None
+        self.linear_acceleration      = None
+        self.lidar_scan_rate          = None
+        self.lidar_range_array        = None
+        self.lidar_intensity_array    = None
+        self.front_camera_image       = None
+        self.rear_camera_image        = None
+        # Vehicle commands
+        self.throttle_command   = None
+        self.steering_command   = None
+        self.headlights_command = None
+        self.indicators_command = None
+
+    # Parse vehicle sensor data
+    def parse_data(self, data, verbose=False):
+        # Actuator feedbacks
+        self.throttle = float(data[self.id + " Throttle"])
+        self.steering = float(data[self.id + " Steering"])
+        # Wheel encoders
+        self.encoder_ticks = np.fromstring(data[self.id + " Encoder Ticks"], dtype=int, sep=' ')
+        self.encoder_angles = np.fromstring(data[self.id + " Encoder Angles"], dtype=float, sep=' ')
+        # IPS
+        self.position = np.fromstring(data[self.id + " Position"], dtype=float, sep=' ')
+        # IMU
+        self.orientation_quaternion = np.fromstring(data[self.id + " Orientation Quaternion"], dtype=float, sep=' ')
+        self.orientation_euler_angles = np.fromstring(data[self.id + " Orientation Euler Angles"], dtype=float, sep=' ')
+        self.angular_velocity = np.fromstring(data[self.id + " Angular Velocity"], dtype=float, sep=' ')
+        self.linear_acceleration = np.fromstring(data[self.id + " Linear Acceleration"], dtype=float, sep=' ')
+        # LIDAR
+        self.lidar_scan_rate = float(data[self.id + " LIDAR Scan Rate"])
+        self.lidar_range_array = np.fromstring(data[self.id + " LIDAR Range Array"], dtype=float, sep=' ')
+        self.lidar_intensity_array = np.fromstring(data[self.id + " LIDAR Intensity Array"], dtype=float, sep=' ')
+        # Cameras
+        self.front_camera_image = cv2.cvtColor(np.asarray(Image.open(BytesIO(base64.b64decode(data[self.id + " Front Camera Image"])))), cv2.COLOR_RGB2BGR)
+        self.rear_camera_image = cv2.cvtColor(np.asarray(Image.open(BytesIO(base64.b64decode(data[self.id + " Rear Camera Image"])))), cv2.COLOR_RGB2BGR)
+        if verbose:
+            print('\n--------------------------------')
+            print('Receive Data from Vehicle: ' + self.id)
+            print('--------------------------------\n')
+            # Monitor vehicle data
+            print('Throttle: {}'.format(self.throttle))
+            print('Steering: {}'.format(self.steering))
+            print('Encoder Ticks:  {} {}'.format(self.encoder_ticks[0],self.encoder_ticks[1]))
+            print('Encoder Angles: {} {}'.format(self.encoder_angles[0],self.encoder_angles[1]))
+            print('Position: {} {} {}'.format(self.position[0],self.position[1],self.position[2]))
+            print('Orientation [Quaternion]: {} {} {} {}'.format(self.orientation_quaternion[0],self.orientation_quaternion[1],self.orientation_quaternion[2],self.orientation_quaternion[3]))
+            print('Orientation [Euler Angles]: {} {} {}'.format(self.orientation_euler_angles[0],self.orientation_euler_angles[1],self.orientation_euler_angles[2]))
+            print('Angular Velocity: {} {} {}'.format(self.angular_velocity[0],self.angular_velocity[1],self.angular_velocity[2]))
+            print('Linear Acceleration: {} {} {}'.format(self.linear_acceleration[0],self.linear_acceleration[1],self.linear_acceleration[2]))
+            print('LIDAR Scan Rate: {}'.format(self.lidar_scan_rate))
+            print('LIDAR Range Array: \n{}'.format(self.lidar_range_array))
+            print('LIDAR Intensity Array: \n{}'.format(self.lidar_intensity_array))
+            cv2.imshow(self.id + ' Front Camera Preview', cv2.resize(self.front_camera_image, (640, 360)))
+            cv2.imshow(self.id + ' Rear Camera Preview', cv2.resize(self.rear_camera_image, (640, 360)))
+            cv2.waitKey(1)
+
+    # Generate vehicle control commands
+    def generate_commands(self, verbose=False):
+        if verbose:
+            print('\n-------------------------------')
+            print('Transmit Data to Vehicle: ' + self.id)
+            print('-------------------------------\n')
+            # Monitor vehicle control commands
+            print('Throttle Command: {}'.format(self.throttle_command))
+            print('Steering Command: {}'.format(self.steering_command))
+            if self.headlights_command == 0:
+                headlights_cmd_str = 'Disabled'
+            elif self.headlights_command == 1:
+                headlights_cmd_str = 'Low Beam'
+            elif self.headlights_command == 2:
+                headlights_cmd_str = 'High Beam'
+            else:
+                headlights_cmd_str = 'Invalid'
+            print('Headlights Command: {}'.format(headlights_cmd_str))
+            if self.indicators_command == 0:
+                indicators_cmd_str = 'Disabled'
+            elif self.indicators_command == 1:
+                indicators_cmd_str = 'Left Turn Indicator'
+            elif self.indicators_command == 2:
+                indicators_cmd_str = 'Right Turn Indicator'
+            elif self.indicators_command == 3:
+                indicators_cmd_str = 'Hazard Indicator'
+            else:
+                indicators_cmd_str = 'Invalid'
+            print('Indicators Command: {}'.format(indicators_cmd_str))
+        return {str(self.id) + ' Throttle': str(self.throttle_command), str(self.id) + ' Steering': str(self.steering_command), str(self.id) + ' Headlights': str(self.headlights_command), str(self.id) + ' Indicators': str(self.indicators_command)}
+
+################################################################################
+
+# Traffic light class
+class TrafficLight:
+    def __init__(self):
+        # Traffic light data
+        self.id    = None
+        self.state = None
+        # Traffic light command
+        self.command = None
+
+    # Parse traffic light data
+    def parse_data(self, data, verbose=False):
+        # Traffic light state
+        self.state = int(data[self.id + " State"])
+        if verbose:
+            print('\n--------------------------------------')
+            print('Receive Data from Traffic Light: ' + self.id)
+            print('--------------------------------------\n')
+            # Monitor traffic light data
+            if self.state == 0:
+                state_str = 'Disabled'
+            elif self.state == 1:
+                state_str = 'Red'
+            elif self.state == 2:
+                state_str = 'Yellow'
+            elif self.state == 3:
+                state_str = 'Green'
+            else:
+                state_str = 'Invalid'
+            print('Traffic Light State: {}'.format(state_str))
+
+    # Generate traffic light control commands
+    def generate_commands(self, verbose=False):
+        if verbose:
+            print('\n-------------------------------------')
+            print('Transmit Data to Traffic Light: ' + self.id)
+            print('-------------------------------------\n')
+            # Monitor traffic light control commands
+            if self.command == 0:
+                command_str = 'Disabled'
+            elif self.command == 1:
+                command_str = 'Red'
+            elif self.command == 2:
+                command_str = 'Yellow'
+            elif self.command == 3:
+                command_str = 'Green'
+            else:
+                command_str = 'Invalid'
+            print('Traffic Light Command: {}'.format(command_str))
+        return {str(self.id) + ' State': str(self.command)}
